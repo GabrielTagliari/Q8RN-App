89b0667a31acf6a7ba332815e0e84a474594ddc1
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2017-03-03 19:56:09 -0300

    Versão 1 - q8rn

---
+++ app/src/main/java/q8rn/com/q8rn/MainActivity.java
@@ -0,0 +1,13 @@
+package q8rn.com.q8rn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
