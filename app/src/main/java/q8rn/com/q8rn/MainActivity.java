6e915e7c7cafba4848f447a70844ff66328e7f4c
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2017-03-03 23:58:57 -0300

    Splash Screen e Activities

--- app/src/main/java/q8rn/com/q8rn/MainActivity.java
+++ app/src/main/java/q8rn/com/q8rn/MainActivity.java
@@ -1,13 +1,28 @@
 package q8rn.com.q8rn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRealizaQuestionario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoRealizaQuestionario = (Button) findViewById(R.id.botaoMainId);
+
+        botaoRealizaQuestionario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FormActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
