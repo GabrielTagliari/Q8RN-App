11eff5413e58ac703a06a2b3f44a56fa4b2a0456
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2017-03-15 23:21:48 -0300

    Funcionando export para CSV

--- app/src/main/java/q8rn/com/q8rn/activities/MainActivity.java
+++ app/src/main/java/q8rn/com/q8rn/activities/MainActivity.java
@@ -2,8 +2,12 @@ package q8rn.com.q8rn.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,11 +26,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import q8rn.com.q8rn.R;
 import q8rn.com.q8rn.constants.Constants;
 import q8rn.com.q8rn.controllers.QuestaoEntrevistadoController;
+import q8rn.com.q8rn.model.CriaBanco;
 import q8rn.com.q8rn.model.PopulaBanco;
 import q8rn.com.q8rn.entities.Questao;
 
@@ -38,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     private Button botaoOffline;
 
     private PopulaBanco populaBanco;
+
+    private CriaBanco banco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,13 +75,22 @@ public class MainActivity extends AppCompatActivity {
         botaoOffline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Intent intent = new Intent(MainActivity.this, QuestionarioActivity.class);
-                intent.putExtra("codQuestao", 1);
-                startActivity(intent);*/
-                QuestaoEntrevistadoController qeController =
-                        new QuestaoEntrevistadoController(getBaseContext());
+                QuestaoEntrevistadoController qe = new QuestaoEntrevistadoController(getBaseContext());
 
-                qeController.findAllQuestaoEntrevistadoByIdEntrevistado(1);
+                File file = qe.gerarExcel();
+                Uri u1  =   null;
+                u1  =   Uri.fromFile(file);
+
+                SimpleDateFormat spf = new SimpleDateFormat("dd/mm/yyyy");
+                String dataAtual = spf.format(new Date());
+
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Q8RN - " + dataAtual);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Banco de dados do questionário dos " +
+                        "oito reméridos naturais gerado em: " + dataAtual);
+                sendIntent.putExtra(Intent.EXTRA_STREAM, u1);
+                sendIntent.setType("text/html");
+                startActivity(sendIntent);
             }
         });
     }
--- app/src/main/java/q8rn/com/q8rn/controllers/QuestaoEntrevistadoController.java
+++ app/src/main/java/q8rn/com/q8rn/controllers/QuestaoEntrevistadoController.java
@@ -5,11 +5,18 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import q8rn.com.q8rn.activities.MainActivity;
 import q8rn.com.q8rn.entities.QuestaoEntrevistado;
 import q8rn.com.q8rn.model.CriaBanco;
 
@@ -73,4 +80,67 @@ public class QuestaoEntrevistadoController {
 
         return resultados;
     }
+
+    public File gerarExcel() {
+        SQLiteDatabase db = banco.getReadableDatabase();
+        Cursor c = null;
+
+            try {
+                c = db.rawQuery("SELECT * FROM " + CriaBanco.TABELA_QUESTAO_ENTREVISTADO
+                        +" WHERE "+ CriaBanco.ENTREVISTADO_ID + " = " + 1
+                        + " ORDER BY " + CriaBanco.QUESTAO_ID + " ASC", null);
+                int rowcount = 0;
+                int colcount = 0;
+                File sdCardDir = Environment.getExternalStorageDirectory();
+                String filename = "MyBackUp.csv";
+                // the name of the file to export with
+                File saveFile = new File(sdCardDir, filename);
+                FileWriter fw = new FileWriter(saveFile);
+
+                BufferedWriter bw = new BufferedWriter(fw);
+                rowcount = c.getCount();
+                colcount = c.getColumnCount();
+                if (rowcount > 0) {
+                    c.moveToFirst();
+
+                    for (int i = 0; i < colcount; i++) {
+                        if (i != colcount - 1) {
+
+                            bw.write(c.getColumnName(i) + ",");
+
+                        } else {
+
+                            bw.write(c.getColumnName(i));
+
+                        }
+                    }
+                    bw.newLine();
+
+                    for (int i = 0; i < rowcount; i++) {
+                        c.moveToPosition(i);
+
+                        for (int j = 0; j < colcount; j++) {
+                            if (j != colcount - 1)
+                                bw.write(c.getString(j) + ",");
+                            else
+                                bw.write(c.getString(j));
+                        }
+                        bw.newLine();
+                    }
+                    bw.flush();
+                    Log.i("excel","Exportado com sucesso");
+                    return saveFile;
+                }
+            } catch (Exception ex) {
+                if (db.isOpen()) {
+                    db.close();
+                    Log.i("excel",ex.getMessage().toString());
+                }
+
+            } finally {
+
+            }
+        return null;
+    }
 }
+
